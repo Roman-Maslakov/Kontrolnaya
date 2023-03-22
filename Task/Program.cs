85d1f9574084a8af44606227f93d93f002d189cf
Ramses <rexid44@gmail.com>
2023-03-22 20:12:33 +1000

    Добавил рабочий метод по созданию массива при помощи ввода пользователя

--- Task/Program.cs
+++ Task/Program.cs
@@ -1,1 +1,23 @@
-﻿
+﻿string[] MakeArray()
+{
+    int n = 0;
+    string[] arr = new string[n];
+    Console.WriteLine("Чтобы добавить слово наберите любой набор символов и нажмите Enter");
+    Console.WriteLine("Чтобы прекратить заполнение массива напишите Stop");
+    while (true)
+    {
+        string word = Console.ReadLine()!;
+        if (word.ToLower() != "stop")
+        {
+            if (word != String.Empty)
+            {
+                Array.Resize(ref arr, n + 1);
+                arr[n] = word;
+                n++;
+            }
+            else Console.WriteLine("Напишите слово");
+        }
+        else break;
+    }
+    return arr;
+}
